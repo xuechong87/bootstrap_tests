3ea1099ed23db9250775bc64cec22e98bc3beee9
宇宙総姐夫 <xuechong87@gmail.com>
2015-11-02 14:35:58 +0800

    增加编码

--- testBuild.py
+++ testBuild.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import re
+import codecs
 
 modeStr = len(sys.argv)>1 and str(sys.argv[1]) or "development"
 
@@ -13,7 +14,7 @@ xmlConfigReplace = "<context-param>\r\n\
 cwd = os.getcwd()
 xmlPath = cwd + os.sep + "WebRoot" + os.sep + "WEB-INF" + os.sep + "web.xml"
 
-webxml = open(xmlPath,"r+")
+webxml = codecs.open(xmlPath,"r+","utf-8")
 webxmlStr = webxml.read()
 
 regStr = "<context-param>\s*<param-name>spring.profiles.default</param-name>\s*<param-value>\S*</param-value>\s*</context-param>"
@@ -22,7 +23,7 @@ reg = re.compile(regStr)
 
 resultXml = webxmlStr.replace(reg.search(webxmlStr).group(),xmlConfigReplace)
 webxml.close()
-targetXml = open(xmlPath,"w+")
+targetXml = codecs.open(xmlPath,"w+","utf-8")
 targetXml.write(resultXml)
 targetXml.close()
 
