62399ae21199111e164296904fa6ca7fe2626a15
宇宙総姐夫 <xuechong87@gmail.com>
2015-11-02 10:42:42 +0800

    Update PyTests.java

--- src/test/test/PyTests.java
+++ src/test/test/PyTests.java
@@ -4,10 +4,10 @@ import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 
 
-import org.python.util.PythonInterpreter;
+//import org.python.util.PythonInterpreter;
 
-import com.xuechong.bootstraptests.py.factory.PYObjectFactory;
-import com.xuechong.bootstraptests.py.interfaces.PyInterface;
+//import com.xuechong.bootstraptests.py.factory.PYObjectFactory;
+//import com.xuechong.bootstraptests.py.interfaces.PyInterface;
 
 public class PyTests {
 	public static void main(String[] args) throws FileNotFoundException, URISyntaxException, InstantiationException, IllegalAccessException {
